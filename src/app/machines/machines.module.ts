5901667523601110bb9a06abb424388860776d9e
C.Manuel Hernández <cmanuelhm87@gmail.com>
2025-01-20 12:21:06 +0100

    feat: basic login as a pass page

--- src/app/machines/machines.module.ts
+++ src/app/machines/machines.module.ts
@@ -5,17 +5,10 @@ import { MachinesRoutingModule } from './machines-routing.module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewMachineComponent } from './pages/new-machine/new-machine.component';
-
+import { PrimengModule } from '../primeng/primeng.module';
 
 @NgModule({
-  declarations: [
-    LayoutPageComponent,
-    ListPageComponent,
-    NewMachineComponent
-  ],
-  imports: [
-    CommonModule,
-    MachinesRoutingModule
-  ]
+  declarations: [LayoutPageComponent, ListPageComponent, NewMachineComponent],
+  imports: [CommonModule, MachinesRoutingModule, PrimengModule],
 })
-export class MachinesModule { }
+export class MachinesModule {}
