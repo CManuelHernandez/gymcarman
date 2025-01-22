0899e558778b020c15e72afd1fdd325e6df32c72
C.Manuel Hernández <cmanuelhm87@gmail.com>
2025-01-22 16:22:55 +0100

    refactor: move variables from edit-machine component to a external file

--- src/app/machines/pages/edit-machine/edit-machine.component.ts
+++ src/app/machines/pages/edit-machine/edit-machine.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { machinesMock } from '../list-page/machinesMock';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  MAIN_WEIGHT_OPTIONS,
+  MAIN_WEIGHT_WHITE_OPTIONS,
+  EXTRA_WEIGHT_OPTIONS,
+  REST_OPTIONS,
+  FEEDBACK_OPTIONS,
+} from './machine-options.constants';
 
 @Component({
   selector: 'app-edit-machine',
@@ -13,59 +20,11 @@ export class EditMachineComponent implements OnInit {
   machine: any;
   machineForm!: FormGroup;
 
-  mainWeightOptions = [
-    { label: '5 Kg', value: 5 },
-    { label: '12.5 Kg', value: 12.5 },
-    { label: '20.0 Kg', value: 20.0 },
-    { label: '27.5 Kg', value: 27.5 },
-    { label: '35.0 Kg', value: 35.0 },
-    { label: '42.5 Kg', value: 42.5 },
-    { label: '50.0 Kg', value: 50.0 },
-    { label: '57.5 Kg', value: 57.5 },
-    { label: '65.0 Kg', value: 65.0 },
-    { label: '72.5 Kg', value: 72.5 },
-    { label: '80.0 Kg', value: 80.0 },
-    { label: '87.5 Kg', value: 87.5 },
-    { label: '95.0 Kg', value: 95.0 },
-    { label: '102.5 Kg', value: 102.5 },
-    { label: '110.0 Kg', value: 110.0 },
-    { label: '117.5 Kg', value: 117.5 },
-    { label: '125.0 Kg', value: 125.0 },
-    { label: '132.5 Kg', value: 132.5 },
-    { label: '140.0 Kg', value: 140.0 },
-    { label: '147.5 Kg', value: 147.5 },
-    { label: '155.0 Kg', value: 155.0 },
-    { label: '162.5 Kg', value: 162.5 },
-    { label: '170.0 Kg', value: 170.0 },
-    { label: '177.5 Kg', value: 177.5 },
-    { label: '185.0 Kg', value: 185.0 },
-    { label: '192.5 Kg', value: 192.5 },
-  ];
-  mainWeightWitheOptions = [
-    { label: '0 (0 Kg)', value: 0 },
-    { label: '1 (5 Kg)', value: 5 },
-    { label: '2 (10 Kg)', value: 10 },
-    { label: '3 (15 Kg)', value: 15 },
-    { label: '4 (20 Kg)', value: 20 },
-    { label: '5 (25 Kg)', value: 25 },
-    { label: '6 (30 Kg)', value: 30 },
-    { label: '7 (35 Kg)', value: 35 },
-    { label: '8 (40 Kg)', value: 40 },
-    { label: '9 (45 Kg)', value: 45 },
-    { label: '10 (50 Kg)', value: 50 },
-    { label: '11 (55 Kg)', value: 55 },
-    { label: '12 (60 Kg)', value: 60 },
-  ];
-  extraWeightOptions = [
-    { label: '0 Kg', value: 0 },
-    { label: '2.5 Kg', value: 2.5 },
-    { label: '5 Kg', value: 5.0 },
-    0,
-    2.5,
-    5.0,
-  ];
-  restOptions = [30, 60, 90, 120];
-  feedbackOptions = ['Mantener peso', 'Subir peso', 'Bajar peso'];
+  mainWeightOptions = MAIN_WEIGHT_OPTIONS;
+  mainWeightWitheOptions = MAIN_WEIGHT_WHITE_OPTIONS;
+  extraWeightOptions = EXTRA_WEIGHT_OPTIONS;
+  restOptions = REST_OPTIONS;
+  feedbackOptions = FEEDBACK_OPTIONS;
 
   constructor(
     private fb: FormBuilder,
