29c65f67b04497a2a58fbd1066024b5ceab2d56c
C.Manuel Hernández <cmanuelhm87@gmail.com>
2025-01-22 16:16:53 +0100

    feat: implement logic to machineType whites

--- src/app/machines/pages/edit-machine/edit-machine.component.ts
+++ src/app/machines/pages/edit-machine/edit-machine.component.ts
@@ -14,11 +14,56 @@ export class EditMachineComponent implements OnInit {
   machineForm!: FormGroup;
 
   mainWeightOptions = [
-    5, 12.5, 20.0, 27.5, 35.0, 42.5, 50.0, 57.5, 65.0, 72.5, 80.0, 87.5, 95.0,
-    102.5, 110.0, 117.5, 125.0, 132.5, 140.0, 147.5, 155.0, 162.5, 170.0, 177.5,
-    185.0, 192.5,
+    { label: '5 Kg', value: 5 },
+    { label: '12.5 Kg', value: 12.5 },
+    { label: '20.0 Kg', value: 20.0 },
+    { label: '27.5 Kg', value: 27.5 },
+    { label: '35.0 Kg', value: 35.0 },
+    { label: '42.5 Kg', value: 42.5 },
+    { label: '50.0 Kg', value: 50.0 },
+    { label: '57.5 Kg', value: 57.5 },
+    { label: '65.0 Kg', value: 65.0 },
+    { label: '72.5 Kg', value: 72.5 },
+    { label: '80.0 Kg', value: 80.0 },
+    { label: '87.5 Kg', value: 87.5 },
+    { label: '95.0 Kg', value: 95.0 },
+    { label: '102.5 Kg', value: 102.5 },
+    { label: '110.0 Kg', value: 110.0 },
+    { label: '117.5 Kg', value: 117.5 },
+    { label: '125.0 Kg', value: 125.0 },
+    { label: '132.5 Kg', value: 132.5 },
+    { label: '140.0 Kg', value: 140.0 },
+    { label: '147.5 Kg', value: 147.5 },
+    { label: '155.0 Kg', value: 155.0 },
+    { label: '162.5 Kg', value: 162.5 },
+    { label: '170.0 Kg', value: 170.0 },
+    { label: '177.5 Kg', value: 177.5 },
+    { label: '185.0 Kg', value: 185.0 },
+    { label: '192.5 Kg', value: 192.5 },
   ];
-  extraWeightOptions = [0, 2.5, 5.0];
+  mainWeightWitheOptions = [
+    { label: '0 (0 Kg)', value: 0 },
+    { label: '1 (5 Kg)', value: 5 },
+    { label: '2 (10 Kg)', value: 10 },
+    { label: '3 (15 Kg)', value: 15 },
+    { label: '4 (20 Kg)', value: 20 },
+    { label: '5 (25 Kg)', value: 25 },
+    { label: '6 (30 Kg)', value: 30 },
+    { label: '7 (35 Kg)', value: 35 },
+    { label: '8 (40 Kg)', value: 40 },
+    { label: '9 (45 Kg)', value: 45 },
+    { label: '10 (50 Kg)', value: 50 },
+    { label: '11 (55 Kg)', value: 55 },
+    { label: '12 (60 Kg)', value: 60 },
+  ];
+  extraWeightOptions = [
+    { label: '0 Kg', value: 0 },
+    { label: '2.5 Kg', value: 2.5 },
+    { label: '5 Kg', value: 5.0 },
+    0,
+    2.5,
+    5.0,
+  ];
   restOptions = [30, 60, 90, 120];
   feedbackOptions = ['Mantener peso', 'Subir peso', 'Bajar peso'];
 
@@ -43,17 +88,24 @@ export class EditMachineComponent implements OnInit {
       machinesMock.find((m) => m.id === this.machineId);
 
     if (this.machine) {
-      // Asegúraarse de que todos los datos existen antes de crear el formulario
       this.machine.machineRutineData = this.machine.machineRutineData || {};
       this.machine.machineRutineData.weight = this.machine.machineRutineData
         .weight || { mainWeight: 5, extraWeight: 0 };
+
+      /* if (this.machine.machineType === 'white') {
+        this.machine.machineRutineData.weight.mainWeight = 0;
+      } */
+
       this.createForm();
     } else {
       console.error('Máquina no encontrada');
     }
+    console.log(this.machine);
   }
 
   createForm() {
+    const isWhiteMachine = this.machine.machineType === 'white';
+
     this.machineForm = this.fb.group({
       lastSeason: [
         this.machine.machineRutineData.lastSeason || 'Sin session',
@@ -61,11 +113,17 @@ export class EditMachineComponent implements OnInit {
       ],
       weight: this.fb.group({
         mainWeight: [
-          this.machine.machineRutineData.weight.mainWeight || 5,
+          this.machine.machineRutineData.weight.mainWeight ||
+            (!isWhiteMachine ? 5 : 0),
           Validators.required,
         ],
         extraWeight: [
-          this.machine.machineRutineData.weight.extraWeight || 0,
+          {
+            value: isWhiteMachine
+              ? 0
+              : this.machine.machineRutineData.weight.extraWeight || 0,
+            disabled: isWhiteMachine,
+          },
           Validators.required,
         ],
       }),
@@ -77,7 +135,6 @@ export class EditMachineComponent implements OnInit {
         this.machine.machineRutineData.repetitions || 5,
         [Validators.required, Validators.min(0)],
       ],
-
       restTime: [
         this.machine.machineRutineData.restTime || 30,
         Validators.required,
@@ -100,7 +157,7 @@ export class EditMachineComponent implements OnInit {
         everDone: true,
         machineRutineData: {
           ...this.machine.machineRutineData,
-          ...this.machineForm.value,
+          ...this.machineForm.getRawValue(),
           lastSeason: formattedDate,
         },
       };
--- src/app/machines/pages/list-page/machinesMock.ts
+++ src/app/machines/pages/list-page/machinesMock.ts
@@ -1,10 +1,10 @@
-const machineWitoutRutineData = {
+const machineWithoutRutineData = {
   lastSeason: '2021-01-01',
   weight: { mainWeight: 5, extraWeight: 0 },
   series: 1,
   repetitions: 5,
   restTime: 30,
-  feedback: 'bien',
+  feedback: 'Mantener peso',
 };
 
 export const machinesMock = [
@@ -15,7 +15,12 @@ export const machinesMock = [
     machineType: 'black',
     everDone: false,
     machineRutineData: {
-      machineWitoutRutineData,
+      lastSeason: '2021-01-01',
+      weight: { mainWeight: 5, extraWeight: 0 },
+      series: 1,
+      repetitions: 5,
+      restTime: 30,
+      feedback: 'Mantener peso',
     },
   },
   {
@@ -25,7 +30,27 @@ export const machinesMock = [
     machineType: 'black',
     everDone: false,
     machineRutineData: {
-      machineWitoutRutineData,
+      lastSeason: '2021-01-01',
+      weight: { mainWeight: 5, extraWeight: 0 },
+      series: 1,
+      repetitions: 5,
+      restTime: 30,
+      feedback: 'Mantener peso',
+    },
+  },
+  {
+    id: 3,
+    exercise: 'biceps',
+    image: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
+    machineType: 'white',
+    everDone: false,
+    machineRutineData: {
+      lastSeason: '2021-01-01',
+      weight: { mainWeight: 1, extraWeight: 0 },
+      series: 1,
+      repetitions: 5,
+      restTime: 30,
+      feedback: 'Mantener peso',
     },
   },
 ];
