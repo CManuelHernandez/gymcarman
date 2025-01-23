bf6f8140453ecfbddeca91088f28941d84a41f8e
C.Manuel Hernández <cmanuelhm87@gmail.com>
2025-01-23 11:24:27 +0100

    feat: sidebar basic layout

--- src/app/machines/machines.module.ts
+++ src/app/machines/machines.module.ts
@@ -7,9 +7,17 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewMachineComponent } from './pages/new-machine/new-machine.component';
 import { PrimengModule } from '../primeng/primeng.module';
 import { EditMachineComponent } from './pages/edit-machine/edit-machine.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [LayoutPageComponent, ListPageComponent, NewMachineComponent, EditMachineComponent],
-  imports: [CommonModule, MachinesRoutingModule, PrimengModule],
+  declarations: [
+    LayoutPageComponent,
+    ListPageComponent,
+    NewMachineComponent,
+    EditMachineComponent,
+    SidebarComponent,
+  ],
+  imports: [CommonModule, MachinesRoutingModule, PrimengModule, FormsModule],
 })
 export class MachinesModule {}
